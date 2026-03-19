6e5615a3ebcacb8cd4a93e100ab6dc70b48ce030
tomexgsm-dev <tomexgsm@gmail.com>
2026-03-19 09:11:56 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,19 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
 app.use(express.static(path.join(__dirname)));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.post("/api/chat", (req, res) => {
+  const message = req.body.message || "";
+
+  const reply = "Odpowiedź AI: " + message;
+
+  res.send(reply);
 });
 
 app.listen(PORT, () => {
